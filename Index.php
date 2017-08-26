b2667be002f78800cac641761c042dd1b6a21c17
run134 <31371158+run134@users.noreply.github.com>
2017-08-27 00:07:13 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,2 +1,8 @@
-<?php
-echo "I Am RoBot";
+curl -X GET \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+https://api.line.me/v1/oauth/verify
+
+{
+  "channelId":1350031035,
+  "mid":"ub00b9ac609e51f4707cd86d8e797491b"
+}
